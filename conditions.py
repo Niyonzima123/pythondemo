75e6a6b14dd1670d9eda32b5959f9b24d9aac3b9
Niyonzima123 <niyonzimaisaac89@gmailcom>
2025-05-17 19:05:56 +0200

    Add conditions and sequences

---
+++ conditions.py
@@ -0,0 +1,38 @@
+n=int(input("Enter a number: "))
+if n < 0:
+    print("number is not positive.")
+elif n > 0:
+    print("number is positive value")
+else:
+    print("number is zero")
+    
+    # learn sequences in python.
+name = "niyonzima"
+print(name[2],name[3])
+# Work with List
+#1. create a list
+names=["Isaac","Gad","Cloude","Mama"]
+#Add some thing
+names.append(input("enter new name: "))
+#Sort list
+names.sort()
+print(names)
+#Work with Set
+#1. Create the set.
+s = set()
+
+# Add element to set
+s.add(1)
+s.add(2)
+s.add(6)
+s.add( 7)
+s.add( 9)
+s.add( 11)
+s.add( 7)
+s.add(90)
+s.add( 10)
+s.add( 78)
+# you can remove elements.
+s.remove(10)
+print(s)
+
--- variables.py
+++ variables.py
@@ -1,8 +1,8 @@
-a=28 # This integers.
-b=5.7 #This is float.  
-c="hello" #This is strings.
-d=True #This is boolean.
-e=None # NoneType
+a=28 # This integers.(int)
+b=5.7 #This is float. (float) 
+c="hello" #This is strings.(str)
+d=True #This is boolean.(bool)
+e=None # NoneType (NoneType)
 print("The variable outputs are:", a, b, c, d, e)
 print(f"a = {a}, b = {b}, c = '{c}', d = {d}, e = {e}")
 
