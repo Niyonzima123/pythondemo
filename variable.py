5b26171b7a0ed0f2f28682b576de2a81d07a59a8
Niyonzima123 <niyonzimaisaac89@gmailcom>
2025-05-17 19:08:06 +0200

    Add variable 2 file

---
+++ variable.py
@@ -0,0 +1,6 @@
+name = "Niyonzima isaac"
+ages = 24
+weight =55.7
+ismaried =False
+girlfriends=None
+print(f"my name is {name} ages {ages} weight {weight} mariege {ismaried} the girlfriends {girlfriends}")
