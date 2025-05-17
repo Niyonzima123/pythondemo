81f26b85dd63cad311fb0a659d80007367c0ad01
Niyonzima123 <niyonzimaisaac89@gmailcom>
2025-05-17 16:49:57 +0200

    add nonetpye comment

--- variables.py
+++ variables.py
@@ -2,7 +2,7 @@ a=28 # This integers.
 b=5.7 #This is float.  
 c="hello" #This is strings.
 d=True #This is boolean.
-e=None
+e=None # NoneType
 print("The variable outputs are:", a, b, c, d, e)
 print(f"a = {a}, b = {b}, c = '{c}', d = {d}, e = {e}")
 
