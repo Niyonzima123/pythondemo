1f80d96081dd14f8d7aa604d30d6e569d1e32df9
Niyonzima123 <niyonzimaisaac89@gmailcom>
2025-05-18 11:37:19 +0200

    Add more on list

--- sequence.py
+++ sequence.py
@@ -1,2 +1,13 @@
+
+# Wecan access the characters in string code. 
 name = "Niyonzima"
 print(name[0],name[3])
+#instead use this use the python datastractures:turple,list,dict ,set,etc. Example:
+names = ["Isaac","Boy","Gabriel","Cloude","Immacule"]
+names.append(input("Enter the names:"))
+names.append(input("Enter the names:"))
+names.append(input("Enter the names:"))
+names.append(input("Enter the names:"))
+names.append(input("Enter the names:"))
+names.sort()
+print(names)
