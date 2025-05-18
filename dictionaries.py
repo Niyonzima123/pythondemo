b00d855512baed9f45938f22894b95842483ba44
Niyonzima123 <niyonzimaisaac89@gmailcom>
2025-05-18 12:37:10 +0200

    Add the dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,6 @@
+houses = {"Harry":"Gryffindor","Draco":"Slytherin"}
+print(houses)
+print(f"the Harry house is {houses['Harry']}")
+#second one to assign value
+houses["Hermione"] = "Grrytffindor"
+print(houses["Hermione"])
