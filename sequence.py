44ac710d35b34be4a913597547e0dfe8f4c829b7
Niyonzima123 <niyonzimaisaac89@gmailcom>
2025-05-18 11:41:05 +0200

    Add comment

--- sequence.py
+++ sequence.py
@@ -3,11 +3,14 @@
 name = "Niyonzima"
 print(name[0],name[3])
 #instead use this use the python datastractures:turple,list,dict ,set,etc. Example:
+#Create a list.
 names = ["Isaac","Boy","Gabriel","Cloude","Immacule"]
+# You can prompt user to add names
 names.append(input("Enter the names:"))
 names.append(input("Enter the names:"))
 names.append(input("Enter the names:"))
 names.append(input("Enter the names:"))
 names.append(input("Enter the names:"))
+# You can sort the list
 names.sort()
 print(names)
