be305f4071250cf26d86126f9e2146288c112fd4
Niyonzima123 <niyonzimaisaac89@gmailcom>
2025-05-18 11:51:01 +0200

    Add a set

---
+++ set.py
@@ -0,0 +1,15 @@
+# Create a empty set
+s=set()
+
+# Add elements in set
+a = input("Enter first number:")
+b= input("Enter first number:")
+c = input("Enter first number:")
+d= input("Enter first number:")
+e = input("Enter first number:")
+s.add(a)
+s.add(b)
+s.add(c)
+s.add(d)
+s.add(e)
+print(s)
